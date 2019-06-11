6fe4806d06b2892f1540eeb9fca2a7f2f4660337
League Student <league@iMac-168.attlocal.net>
2019-06-10 18:27:42 -0700

    week20 :smile:
    fun done change calculator and sleepyhead

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -1,31 +1,39 @@
 package doubles_and_booleans;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
 
+//    Level 0
 
 import javax.swing.JOptionPane;
 
 /*
  * I have a pocket full of change. I hate doing math. Make me a program that
- * will calculate how much money I have without me having to use my brain. Then
- * make me a sandwich.
+ * will calculate how much money I have without me having to use my brain. 
  */
 public class ChangeCalculator {
 
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String nick = JOptionPane.showInputDialog("How many Nickels do you have? ");
 		// Convert their answer to an int using Integer.parseInt()
-
+		double els = Integer.parseInt(nick);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String di = JOptionPane.showInputDialog("How many dimes do you have? ");
+		double me = Integer.parseInt(di);
 		// Ask the user how many quarters they have, and convert their answer
-
-		// Calculate how much money the user has and save it in a double variable 
-
+		String quar = JOptionPane.showInputDialog("How many Quarters do you have? ");
+		double ters = Integer.parseInt(quar);
+		// Calculate how much money the user has and save it in a double variable
+		double all = ((els * 5) + (me * 10) + (ters * 25)) / 100;
 		// Tell the user how much money they have
-
+		if (all > 400.00) {
+			JOptionPane.showMessageDialog(null, "You have $" + all + " , you are a billionaire and need to give some of your money to a deserving charity.");
+		}if (all > 4.00&&all <400.00) {
+			JOptionPane.showMessageDialog(null, "You have $" + all + " , you are RICH!!!!!!");
+		} if (all<4.00) {
+			JOptionPane.showMessageDialog(null, "You have $" + all + " , you have spent all your fourtune");
+		}
+		
+		}
 	}
-}
 
--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -1,30 +1,39 @@
 package doubles_and_booleans;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /* You MUST use the above boolean variables in your code */
-        
-        /*
-         * Ask the user for these values using a confirm dialog like the one below
-         * JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-         */
-    
+		boolean isWeekday, isVacation;
 
+		/* You MUST use the above boolean variables in your code */
 
-        /*
-         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!�? If it is a weekday, and we are on vacation,
-         * print “sleep in�?.
-         */
-    }
+		// Ask the user for these values using a confirm dialog like the one below
+		isVacation = JOptionPane.showConfirmDialog(null, "Are you on vacation?", "Sleepy Head",
+				JOptionPane.YES_NO_OPTION) == 0;
+		System.out.println(isVacation);
+		isWeekday = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head",
+				JOptionPane.YES_NO_OPTION) == 0;
+		System.out.println(isWeekday);
+		if (!isWeekday || isVacation) {
+			JOptionPane.showMessageDialog(null, "You should sleep in");
+
+		}
+
+		else if (isWeekday) {
+			JOptionPane.showMessageDialog(null, "Get up lazybones!");
+		}
+		else if (isVacation && isWeekday) {
+			JOptionPane.showMessageDialog(null, " You should sleep in");
+		} /*
+			 * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday, print
+			 * “get up lazybones!�? If it is a weekday, and we are on vacation, print “sleep
+			 * in�?.
+			 */
+	}
 }
