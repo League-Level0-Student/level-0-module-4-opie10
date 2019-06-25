7aa58716c53d5fce0a23dcff2d219f1222470c1e
League Student <league@iMac-168.attlocal.net>
2019-06-24 18:30:41 -0700

    week22 :smile:
    done fruit quiz working on simple calculator

--- src/extra/FruitQuiz.java
+++ src/extra/FruitQuiz.java
@@ -13,15 +13,17 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-public class FruitQuiz extends KeyAdapter {
+public class FruitQuiz extends KeyAdapter{
 
 	void makeQuestions() {
-		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
+	question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
 		// 11. Make another question called "question2".  Use question1 above as a guide.
 	question2 = new JLabel("<html>Which is not a real swimming stroke? <br> A: Backstroke <br> B: Brestroke  <br> C: Sidestroke</html>");
 	question3 = new JLabel("<html>Which is not a real type of dog? <br> A: Catahoula <br> B: Chihuaahau  <br> C: Great dane</html>");
 	question4 = new JLabel("<html>Which is not a real place? <br> A: Westbouough  <br> B: Taumatawhakatangi <br> C: Aceville</html>");
-	question5 = new JLabel("<html>Which  answer is correct? <br> A:All of the Below   <br> B:  None of the Below  <br> C:All of the above <br> D: One Of the above  <br> E: None of the above <br> F: None of the above  </html>");
+	question5 = new JLabel("<html> If you choose an answer to this question at random, what is the chance you will be correct?<br> A: 25% <br> B:  50%  <br> C:0%<br> D: 25% <br> E: None are right <br>F:All are right </html>");
+	question6 = new JLabel("<html> Which book is this the opening line of?'It was a bright cold day in april and the clocks were striking thirteen'<br> A:The Catcher in the Rye, by J.D. Salinger <br> B:1984, by George Orwell <br> C:Catch 22, by Joseph Heller</html>");
+	question7= new JLabel("<html>Which  answer is correct? <br> A:All of the Below   <br> B:  None of the Below  <br> C:All of the above <br> D: One Of the above  <br> E: None of the above <br> F: None of the above  </html>");
 	}
 
 	@Override
@@ -34,23 +36,49 @@ System.out.println(keyCode);
 		int B = 66;
 		int C = 67;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
-		if (question5.isShowing()) {
-			if(keyCode == ) {
+		if (question7.isShowing()) {
+			if(keyCode == 69) {
 				correct();
-				
+				JOptionPane.showMessageDialog(null, "You got to question 7, you win!! You are also the smartest person ever ");
 			}
 				else {
 				incorrect();	
+				JOptionPane.showMessageDialog(null, "You got to question 7");
+				System.exit(0);
+				}
+			}
+		if (question6.isShowing()) {
+			if(keyCode == 66) {
+				correct();
+				nextQuestion(question7);
+			}
+				else {
+				incorrect();	
+				JOptionPane.showMessageDialog(null, "You got to question 6 ");
+				System.exit(0);
+				}
+			}
+		if (question5.isShowing()) {
+			if(keyCode == 69) {
+				correct();
+				nextQuestion(question6) ;	
+			}
+				else {
+				incorrect();	
+				JOptionPane.showMessageDialog(null, "You got to question 5");
+				System.exit(0);
+				
 				}
 			}
 		if (question4.isShowing()) {
 			if(keyCode == 65) {
 				correct();
-				
+				nextQuestion(question5) ;
 			}
 				else {
 				incorrect();	
-				}
+				JOptionPane.showMessageDialog(null, "You got to question 4");
+				System.exit(0);}
 			}
 		else if (question3.isShowing()) {
 			if(keyCode == 66) {
@@ -59,7 +87,8 @@ System.out.println(keyCode);
 			}
 				else  {
 				incorrect();	
-				}
+				JOptionPane.showMessageDialog(null, "You got to question 3");
+				System.exit(0);}
 			}
 		// 12. If question2 is showing,
 		else if (question2.isShowing()) {
@@ -69,7 +98,8 @@ System.out.println(keyCode);
 			}	// 13. check if it is right or wrong like you did for question1
 		else {
 			incorrect();
-		}
+			JOptionPane.showMessageDialog(null, "You got to question 2");
+			System.exit(0);}
 		}	
 		else if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
@@ -87,9 +117,10 @@ System.out.println(keyCode);
 			// 8. else (if they touched something else)
 			else {
 				incorrect();
-			}
+				JOptionPane.showMessageDialog(null, "You got to question 1");
+				System.exit(0);
 				// 9. Call the incorrect() method
-		
+			}
 		}
 
 	}
@@ -100,7 +131,7 @@ System.out.println(keyCode);
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
 
-playSound("chime.wav");
+playSound("correct.wav");
 	}
 
 	private void incorrect() {
@@ -114,6 +145,8 @@ playSound("wrong.wav");
 		frame.remove(question3);
 		frame.remove(question4);
 		frame.remove(question5);
+		frame.remove(question6);
+		frame.remove(question7);
 		frame.add(newQuestion);
 		frame.pack();
 		frame.setVisible(true);
@@ -140,6 +173,6 @@ playSound("wrong.wav");
 	}
 
 	JLabel question1 = new JLabel(), question2 = new JLabel(), question3 = new JLabel(), question4 = new JLabel(),
-			question5;
+			question5= new JLabel(),question6= new JLabel(),question7;
 }
 
--- src/extra/SimpleCalculator.java
+++ src/extra/SimpleCalculator.java
@@ -1,26 +1,47 @@
 package extra;
 
-
 import javax.swing.JOptionPane;
 
 public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
+		String number1 = JOptionPane.showInputDialog("Give me a number or else");
+		String number2 = JOptionPane.showInputDialog("Give me another number or else"); // 2. Customize pop-up to
+																						// support
+																						// add/subtract/multiply/divide
+																						// operations.
+		int operation = JOptionPane.showOptionDialog(null,
+				"What opereration do you want to do with " + number1 + " and " + number2 + "?", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Add", "Subtract", "Multiply ", "Divide" }, null);
 
-		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
-		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
-				null);
-
-		// 5. Call the methods created in steps 3 and 4 to perform the appropriate operation.
-
+		// 5. Call the methods created in steps 3 and 4 to perform the appropriate
+		// operation.
+double one =Double.parseDouble(number1);
+double two=Double.parseDouble(number2);
+Add(one,two);
+Subtract(one,two);
+Multiply(one,two);
+Divide(one,two);
 		// 6. Wrap steps 1, 2 and 5 in a for loop to repeat calculator operations
 	}
 
 	// 3. Create method for addition operation.
-	//    Method should create pop-up that shows equation and solution, eg. 45 + 25 = 70.
-	//    HINT: use 'static void add(int num1, num2) { ... }
+	// Method should create pop-up that shows equation and solution, eg. 45 + 25 =
+	// 70.
+	// HINT: use 'static void add(int num1, num2) { ... }
+	static double Add(double number1, double number2) {
 
+		return number1 + number2;
+	}static double Subtract(double number1, double number2) {
+
+		return number1 - number2;
+	}static double Divide(double number1, double number2) {
+
+		return number1 / number2;
+	}static double Multiply(double number1, double number2) {
+
+		return number1 * number2;
+	}
 	// 4. Create similar methods for subtraction, multiplication and division.
 }
